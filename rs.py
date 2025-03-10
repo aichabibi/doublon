915ac74904954afd445ccf84104b780bdcbaa5cf
aicha <117689374+aichabibi@users.noreply.github.com>
2025-03-10 12:52:17 +0100

    Update rs.py

--- rs.py
+++ rs.py
@@ -36,6 +36,9 @@ def detect_duplicates(df):
     # Convertir la colonne des dates en format datetime
     df[col_date] = pd.to_datetime(df[col_date], errors='coerce')  # Convertir en format date
     
+    # Formater la colonne des dates pour afficher uniquement la date sans l'heure
+    df[col_date] = df[col_date].dt.strftime('%Y-%m-%d')  # Format : 'YYYY-MM-DD'
+    
     # Détecter les doublons en fonction du matricule et de la date
     duplicate_df = df[df.duplicated(subset=[col_matricule, col_date], keep=False)]
     
@@ -58,6 +61,7 @@ def detect_duplicates(df):
         )
     else:
         st.success("Aucun doublon trouvé.")
+
 
 # Interface principale de l'application
 def main():
